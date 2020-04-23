a3189226bdd305cbf448187e276abd3c5c57f184
d3faultik <sheremetyev48@mail.ru>
2020-04-23 17:20:36 +0300

    WordCheckFragment Commit -добавление фрагмента для проверки слов

--- app/src/main/java/com/develop/vadim/english/Basic/WordCheckActivity.java
+++ app/src/main/java/com/develop/vadim/english/Basic/WordCheckActivity.java
@@ -195,8 +195,6 @@ public class WordCheckActivity extends AppCompatActivity {
             userAnswerEditText.startAnimation(alphaAnimation);
             userQuestionTextView.startAnimation(alphaAnimation);
             forgetWordTextView.startAnimation(alphaAnimation);
-
-
         }
         else {
             userAnswerEditText.setTextColor(getResources().getColor(R.color.wrongWordRed));
